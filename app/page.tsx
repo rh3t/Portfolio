584362917ba5074d5fcba1246e170afa9a9469df
Keegan <meddakee2000@gmail.com>
2025-07-27 08:09:04 -0400

    smmar

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,30 @@ const Index = () => {
 
         <>
 
+<<<<<<< HEAD
             Hello, world!
         
         </>
+=======
+            <LandingIntro></LandingIntro>
+
+            <div style = {{marginTop: 25, gap: 25}} className = 'flexbox-centered'>
+
+                <RedirectButton iconSrc = '/images/github.svg' text = 'GitHub' url = 'https://github.com/rh3t'/>
+                <RedirectButton iconSrc = 'images/twitter.svg' text = 'Twitter' url = 'https://x.com/rh3t_'/>
+
+            </div>
+
+            <div style = {{marginTop: 25, gap: 25}} className = 'flexbox-centered'>
+
+                <RedirectButton iconSrc = '/images/monkeytype.svg' text = 'MonkeyType' url = 'https://monkeytype.com/profile/Rh3t_'/>
+                <RedirectButton iconSrc = '/images/discord.svg' text = 'Discord' url = '/'/>
+
+
+            </div>
+
+        </div>
+>>>>>>> parent of d227463 (Commit v1.1.5)
 
     )
 }
